043e1641013020542cf52d87bdb5ebb746d83812
GAMALIEL <lic.camey@gmail.com>
2024-09-16 18:30:57 +0100

    HW 8 - React Intro

---
+++ front/src/views/Profile/index.jsx
@@ -0,0 +1,12 @@
+import Appoinments from "../../components/Appoinments";
+
+function Profile() {
+  return (
+    <div>
+      <h1>My Profile</h1>
+      <Appoinments />
+    </div>
+  );
+}
+
+export default Profile;
