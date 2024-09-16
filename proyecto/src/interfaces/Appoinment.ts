0aab2e274712c27d4e694f11cdf8a7dfb94812dc
GAMALIEL <lic.camey@gmail.com>
2024-09-16 13:23:32 +0100

    HW3 TS-Express

--- proyecto/src/interfaces/Appoinment.ts
+++
@@ -1,9 +0,0 @@
-import {User} from "./User";
-
-export interface Appoinment {
-  id: number;
-  date: string;
-  time: string;
-  status?: string;
-  user?: User;
-}
--- proyecto/src/services/usersService.ts
+++ proyecto/src/services/usersService.ts
@@ -1,64 +1,15 @@
-import {Credentials} from "../interfaces/Credentials";
-import {User} from "../interfaces/User";
-
-export const getAllUsersService = async (): Promise<User[]> => {
-  const users: User[] = [
-    {
-      name: "Bartolomiau",
-      email: "barto@gmail.com",
-      address: "argentina",
-      phone: "123456",
-      profile_pic: "laurl.jpg",
-      credentials: {email: "barto@gmail.com", password: "12345"},
-      appointments: [],
-    },
-  ];
-  return await users;
+export const getAllUsersService = async () => {
+  return await "Devolver todos los usuarios";
 };
 
-export const getUserByIdService = async (
-  id: number
-): Promise<User | undefined> => {
-  const user: User | undefined = {
-    id: 1,
-    name: "Bartolomiau",
-    email: "barto@gmail.com",
-    address: "argentina",
-    phone: "123456",
-    profile_pic: "laurl.jpg",
-    credentials: {email: "barto@gmail.com", password: "12345"},
-    appointments: [],
-  };
-
-  return await user;
+export const getUserByIdService = async (id: number) => {
+  return await "Devolver usuario por id";
 };
 
-export const createUserService = async (user: User): Promise<User> => {
-  const newUser: User = {
-    name: "Bartolomiau",
-    email: "barto@gmail.com",
-    address: "argentina",
-    phone: "123456",
-    profile_pic: "laurl.jpg",
-    credentials: {email: "barto@gmail.com", password: "12345"},
-  };
-
-  return await newUser;
+export const createUserService = async (user: any) => {
+  return await "Crear usuario";
 };
 
-export const loginService = async (
-  credentials: Credentials
-): Promise<User | null> => {
-  const user: User | undefined = {
-    id: 1,
-    name: "Bartolomiau",
-    email: "barto@gmail.com",
-    address: "argentina",
-    phone: "123456",
-    profile_pic: "laurl.jpg",
-    credentials: {email: "barto@gmail.com", password: "12345"},
-    appointments: [],
-  };
-
-  return await user;
+export const loginService = async (credentials: any) => {
+  return await "Loguear usuario";
 };
